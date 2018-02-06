217ba787fbaf3edca9fa279af2e12c7ef7de32f5
iawasthi <resonanceoncl@gmail.com>
2018-02-05 18:45:01 -0600

    While loop

---
+++ app/experiment.php
@@ -0,0 +1,11 @@
+<?php  
+
+$names = array('Brad', 'John', 'Jack', 'Bob', 'Rob');
+
+$count = 0;
+
+while ($count < count($names)){
+?> <h1>The names of the people are
+<?php $names[count] }?> 
+</h1>
+
---
+++ app/wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,7 @@
+<?php  
+function myFirstFunction($name, $color){
+   echo "My name is $name, and my favorite color is $color ";
+}
+?>
+
+myFirstFunction("John", "green");
