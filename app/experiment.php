d8035e8381914b68dae13988fe33699007c83440
iawasthi <resonanceoncl@gmail.com>
2018-02-07 17:16:17 -0600

    end lecture 13

--- app/experiment.php
+++ app/experiment.php
@@ -4,8 +4,42 @@ $names = array('Brad', 'John', 'Jack', 'Bob', 'Rob');
 
 $count = 0;
 
-while ($count < count($names)){
-?> <h1>The names of the people are
-<?php $names[count] }?> 
-</h1>
+while ($count < count($names))
+{
+   echo "<h1> The name is $names[$count]   </h1>";
+   $count++;
+}
+?> 
 
+
+<?php  
+/*
+while(have_posts()){
+  
+   ?> <h2> <?php
+          echo bloginfo('title'); ?>
+      </h2> 
+<h2> <?php
+          echo bloginfo('admin_email'); ?>
+      </h2>
+
+<h2> <?php
+          $b= get_bloginfo('template_directory'); 
+      echo "get_bloginfo: $b" ;?>
+      </h2>
+<h2> <?php
+          echo bloginfo('template_directory'); ?>
+      </h2> 
+
+<h2> <?php
+          echo bloginfo('template_url'); ?>
+      </h2> 
+<h2> <?php
+          echo bloginfo('version'); ?>
+      </h2> 
+   
+   <?php
+   the_post(); break;
+}
+*/
+?>
